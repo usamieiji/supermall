f7de9e1fd554ef8b79a5c0cc552aa97032e2be00
usamieiji <mrakira@outlook.com>
2020-03-28 14:15:16 +0800

    delete request.js

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,43 +3,97 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banner="banner"></home-swiper>
+    <home-swiper :banner="banner" class="home-swiper"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :title="['流行', '新款', '精选']" class="tab-control"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
 
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar"
-  import HomeSwiper from "./childComps/HomeSwiper"
 
-  import {getHomeMultidata} from "network/home"
+  import HomeSwiper from "./childComps/HomeSwiper"
+  import RecommendView from "./childComps/RecommendView"
+  import FeatureView from "./childComps/FeatureView"
+
+  import NavBar from "components/common/navbar/NavBar"
+  import TabControl from "components/content/tabControl/TabControl"
+  import GoodsList from "components/content/goods/GoodsList"
+
+  import {getHomeMultidata, getHomeGoods} from "network/home"
 
   export default {
     name: "Home",
     components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
       NavBar,
-      HomeSwiper
+      TabControl,
+      GoodsList
     },
     data() {
       return {
         banner: [],
         recommend: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
 
       }
     },
     created() {
-      getHomeMultidata().then(res => {
-        this.banner = res.data.banner.list
-        this.recommend = res.data.recommend.list
-      })
+      this.getHomeMultidata()
+
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banner = res.data.banner.list
+          this.recommend = res.data.recommend.list
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page
+        getHomeGoods(type, page + 1).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page++
+        })
+      }
     }
 
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+    padding-bottom: 49px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+
+    z-index: 9;
   }
+
+  .tab-control {
+    position: sticky;
+    top: 44px ;
+
+    z-index: 9;
+  }
+
 </style>
